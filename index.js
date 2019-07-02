205ed94ddb255371adc32a38952a01a493b35979
Yann Stoneman <yann@serverlessguru.com>
2019-07-02 13:22:53 -0700

    added delete comment posts by size

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const params = {
-    Bucket: 'medium-html'
-}
+const Bucket = 'medium-html';
+const listParams = { Bucket };
 
 // this Lambda has full permissions for S3's medium-html (specify that in serverless.yml)
 // also backup daily to another bukect to which no one and no service has delete access
 exports.handler = async (event) => {
     const allKeys = [];
     // list objects in medium-html bucket
-    const response = await s3.listObjectsV2(params).promise();
+    const response = await s3.listObjectsV2(listParams).promise();
     response.Contents.forEach(obj => {
+        if (obj.Size < 10000) {
+            const deleteParams = { Bucket, Key: obj.Key };
+            s3.deleteObject(deleteParams).promise(); // S3 is eventually consistent...results may not be immediate.
+        }
         console.log(obj);
         return allKeys.push(obj.Key);
     });
