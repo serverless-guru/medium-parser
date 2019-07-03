005d8d10145e3280695b02e901261dfc4c73c21e
Yann Stoneman <yann@serverlessguru.com>
2019-07-02 21:17:11 -0700

    s3 acl allows public read now

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context, callback) => {
             const stylefree = original.replace(/<style>.*<\/style>/is, '');
 
             // Update S3 with the changes
-            await s3.putObject({ Bucket, Key: key, Body: stylefree }).promise();
+            await s3.putObject({ Bucket, Key: key, Body: stylefree, ACL: 'public-read' }).promise();
         }
 
         callback(null, 'Success!');
@@ -36,13 +36,6 @@ exports.handler = async (event, context, callback) => {
         callback(err.message);
     }
 
-
-
-
-
-
-    // response.Contents.forEach(obj => {
-    //     const objParams = { Bucket, Key: obj.Key };
     //     if (obj.Size < 10000) {
     //         console.log(`Deleting ${obj.Key}, with size of ${obj.Size}...`);
     //         s3.deleteObject(objParams, (err, res) => {
@@ -50,19 +43,6 @@ exports.handler = async (event, context, callback) => {
     //         });
     //         return;
     //     }
-    //     s3.getObject(objParams, function (err, data) {
-    //         // Handle any error and exit
-    //         if (err)
-    //             return err;
-    //         // Convert Body from a Buffer to a String
-    //         const original = data.Body.toString('utf-8'); // Use the encoding necessary
-    //         const stylefree = original.replace(/<style>.*<\/style>/is, '');
-    //         console.log(stylefree)
-    //         // s3.putObject({ Body: stylefree, Bucket, Key: obj.Key, Tagging: "destyled=true" }, function (err, data) {
-    //         //     if (err) console.log(err, err.stack); // an error occurred
-    //         //     else console.log(data);           // successful response
-    //         // });
-    //     });
 
     //     const path = obj.Key.slice(0, -5); // remove '.html'
     //     metaData.push({
@@ -70,16 +50,10 @@ exports.handler = async (event, context, callback) => {
     //     })
     // });
 
-
-    // // get title by finding `<h1 class="p-name">` and deleting everything between that and `</h1>`.
-
     // console.log('printing all metadata:')
     // console.log(metaData);
     // as you iterate through each object,
     // push object meta-data to an array
-    // copy entire object content to a temporary variable
-    // change that temporary variable by taking out everything between and including <style></style>
-    // update the object in S3 with the new value of the temporary value
 
     // update the Index.js (table of contents object) with the updated meta-data array
 }
@@ -108,7 +82,6 @@ exports.handler = async (event, context, callback) => {
 
 // set lambda timeout to 1 minute
 
-
 // {
 //     "Version": "2012-10-17",
 //         "Statement": [
@@ -117,11 +90,10 @@ exports.handler = async (event, context, callback) => {
 //                 "Effect": "Allow",
 //                 "Action": [
 //                     "s3:PutObject",
-//                     "s3:PutObjectTagging"
+//                     "s3:PutObjectTagging",
+//                     "s3:PutObjectAcl"
 //                 ],
-//                 "Resource": "arn:aws:s3:::medium-html/"
+//                 "Resource": "arn:aws:s3:::medium-html/*"
 //             }
 //         ]
 // }
-
-
